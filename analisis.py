3e1e69512a8f881baa35ec9374185d1ad54dae72
StevenSsj1 <lllano@est.ups.edu.ec>
2023-11-19 18:07:17 -0500

    tasa_homicidios metodo

---
+++ analisis.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas import DataFrame
+
+#Lectura del archivo .csv
+poblacion_provincia = [881394, 209933, 281396, 186869, 524004, 488716
+                      ,715751, 643654, 33042, 4387434, 476257]
+reader = pd.read_csv('mdi_homicidiosintencionales_pm_2023_enero_septiembre.csv',
+                     delimiter=';', encoding='ISO-8859-1')
+df = pd.DataFrame(reader)
+row, column = df.shape #numeros de filas y columnas en el csv
+#info = df.info() # informacion sobre el tipo de dato de la columna y sabes si tiene valores nulos
+
+def info_by_canton(canton:str) -> dict:
+    if canton not in df['Cantón'].unique():
+        return 'Error'
+    datos_canton = df[df['Cantón'] == canton][['Tipo Muert.',
+                                                'Probable Causa M.',
+                                                'Arma']].to_dict('records')
+    return datos_canton
+print(info_by_canton('ha'))
+
+def obtener_lista_provincias_con_homicidios(df):
+    # Filtra el DataFrame para incluir solo las filas con 'Tipo Muert.' igual a 'HOMICIDIO'
+    df_homicidios = df[df['Tipo Muert.'] == 'HOMICIDIO']
+    # Utiliza el método unique() para obtener las provincias con homicidios sin repetir
+    lista_provincias_con_homicidios = df_homicidios['Provincia'].unique().tolist()
+    ordenados = sorted(lista_provincias_con_homicidios)
+
+    return ordenados
+
+def combinar_listas_en_dataframe(lista_provincias, lista_poblacion):
+    # Crear un DataFrame con las listas de provincias y población
+    datos = pd.DataFrame({'Provincia': lista_provincias, 'Poblacion': lista_poblacion})
+
+    return datos
+lista_provincias = obtener_lista_provincias_con_homicidios(df)
+poblacion_provincias = poblacion_provincia * (23 // len(poblacion_provincia)) + poblacion_provincia[:23 % len(poblacion_provincia)]
+def tasa_homicidios_provincia():
+    # Agrupa por provincia y cuenta el número de homicidios en cada provincia
+    homicidios_por_provincia = df[df['Tipo Muert.'] == 'HOMICIDIO'].groupby('Provincia').size().reset_index(
+        name='Numero de Homicidios')
+    datos_provincias = combinar_listas_en_dataframe(lista_provincias, poblacion_provincias)
+    datos_combinados = pd.merge(homicidios_por_provincia, datos_provincias, on='Provincia')
+    datos_combinados['Resultado'] = (datos_combinados['Numero de Homicidios'] / datos_combinados['Poblacion'] * 100000)
+    return datos_combinados[['Provincia', 'Resultado']]
+
+print(tasa_homicidios_provincia())
+
+
+# def distribucion_edad():
+#     distribucion_edad = df['Edad'].value_counts().to_dict()
+#     distribucion_edad = {edad: frecuencia for edad, frecuencia in distribucion_edad.items() if edad >= 13}
+#     return distribucion_edad
+# distribucion_edad()
--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from typing import Union
-from lecturaDatos import get_dats_by_head
+from analisis import *
 from fastapi import FastAPI, HTTPException
-
+from fastapi.responses import PlainTextResponse
 app = FastAPI()
 
 
-@app.get("/{headers}")
-def read_root(headers: str):
-    datos = get_dats_by_head(headers)
-    if not datos.empty:
-        return {"mensaje": "Datos obtenidos correctamente", "datos": datos.to_dict(orient='records')}
-    raise HTTPException(status_code=404, detail="Persona no encontrada")
+@app.get("api/{canton}")
+def read_root(canton: str):
+    datos = info_by_canton(canton)
+    if datos != 'Error':
+        return {"mensaje": f"Datos obtenidos correctamente del cantón {canton}", "datos": datos}
+    raise HTTPException(status_code=404, detail="Canton no encontrado")
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get('/api/homicidios')
+async def tasa_homicidios_provincia_endpoint():
+    try:
+        # Llama a la función que calcula las tasas de homicidios por provincia
+        resultado = tasa_homicidios_provincia()
+
+        # Convierte el DataFrame a un formato de cadena y devuelve la respuesta como texto plano
+        respuesta_texto = resultado.to_string(index=False)
+        return PlainTextResponse(content=respuesta_texto, media_type="text/plain")
+    except Exception as e:
+        # Manejo de errores
+        raise HTTPException(status_code=500, detail=f"Error al procesar la solicitud: {str(e)}")
